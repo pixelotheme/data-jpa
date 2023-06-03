2268bfc653fb7147636d65f4af69cedf7e310157
hoan0418 <hoan0418@naver.com>
2023-06-03 16:30:23 +0900

    1. 도메인 모델 생성, 테스트

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,32 +1,47 @@
 package study.datajpa.entity;
 
-import lombok.Generated;
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본생성자 protected level 까지만 가능
+@ToString(of = {"id", "username", "age"}) // 연관관계 team엔티티 넣으면 무한 루프 돌수  있어 제거
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
+    private int age;
 
-    protected Member(){
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
-    }
 
     public Member( String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
     public void changeUserName(String username){
         this.username = username;
+    }
+
+    // =======연관관계 편의 메서드 ============
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
