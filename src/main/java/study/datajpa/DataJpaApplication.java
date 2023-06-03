40ec3785a1377d0ce51350516e1fa3a520aeb33a
hoan0418 <hoan0418@naver.com>
2023-06-03 17:43:23 +0900

    1. SpringDataJPA 공통 인터페이스 적용, 테스트 작성

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,22 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * JavaConfig 설정- 스프링 부트 사용시 생략 가능
+ *
+ * @Configuration
+ * @EnableJpaRepositories(basePackages = "jpabook.jpashop.repository")
+ * public class AppConfig {}
+ *
+ * 스프링 부트 사용시 @SpringBootApplication 어노테이션의 위치를 자동으로 지정(해당 패키지와 하위 패키지 인식)
+ * -> 만약 위치가 달라지면 @EnableJpaRepositories 필요
+ *
+ * */
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.data-jpa.repository")
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
